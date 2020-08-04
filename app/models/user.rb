7d8fc85847d3e36b652a218972c554984a9dc546
Muhammad Fahad Umer <50762557+MFahadUmer@users.noreply.github.com>
2020-08-04 11:30:43 +0500

    Revert "Milestine 4: Deployment"

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,40 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, length: { minimum: 5, maximum: 20 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
-  validates :email, presence: true,
-                    uniqueness: { case_sensitive: false },
-                    format: { with: VALID_EMAIL_REGEX, message: 'The email is not valid' }
-  validates :password, presence: true, length: { minimum: 6, maximum: 32 }
+  validates :name, presence: true, length: { maximum: 20 }
 
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-
-  has_many :friendships
-  has_many :friends_user, class_name: 'Friendship', foreign_key: 'friendship_id'
-  has_many :folks, through: :friends_user, source: :friend
-  # Confirmed Friendship
-  has_many :confirmed_friendships, -> { where status: 'Confirmed' }, class_name: 'Friendship'
-  has_many :confirmed_friends, through: :confirmed_friendships, source: :friend
-  # sending Friend Request
-  has_many :pending_friendships, -> { where status: 'Pending' }, class_name: 'Friendship', foreign_key: 'user_id'
-  has_many :pending_friends, through: :pending_friendships, source: :friend
-  # Received Friend Request
-  has_many :pending_friendship, -> { where status: 'Pending' }, class_name: 'Friendship', foreign_key: 'friendship_id'
-  has_many :pending_received_requests, through: :pending_friendship, source: :user
-
-  def friends_and_own_posts
-    Post.where(user: confirmed_friends).or(Post.where(user: self)).ordered_by_most_recent
-  end
-
-  def mutual_friends
-    friends_array = friendships.map { |friendship| friendship.friendship_id if friendship.status == 'Confirmed' }
-    friends_array.compact
-  end
-
-  def friend?(user)
-    confirmed_friends.include?(user)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
+
   root 'posts#index'
-  # get 'update', to: 'friendships#update', as: :update_friendship
 
   devise_for :users
 
-  resources :users, only: %i[index show]
-  resources :friendships, only: %i[new create destroy edit update]
-  resources :posts, only: %i[index create] do
-    resources :comments, only: %i[create]
-    resources :likes, only: %i[create destroy]
+  resources :users, only: [:index, :show]
+  resources :posts, only: [:index, :create] do
+    resources :comments, only: [:create]
+    resources :likes, only: [:create, :destroy]
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
